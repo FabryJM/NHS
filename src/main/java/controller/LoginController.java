ecdc4a6bdecbbd3f17837b97e26b5a8c5c27db5c
Fynn Luca Peschel <fynn-luca.peschel@hmmh.de>
2021-07-29 14:52:47 +0200

    added code for login() and register() LOL

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -2,6 +2,7 @@ package controller;
 
 import model.User;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
@@ -28,11 +29,25 @@ public class LoginController {
                 inputUsername, inputFirstname, inputLastname, inputDateOfBirth, inputEmail, inputPassword, inputRole));
     }
 
-    public void login() {
+    public void login(Connection connection) throws SQLException {
         Scanner scanner = new Scanner(System.in);
 
         String inputUsername = scanner.nextLine();
         String inputPassword = scanner.nextLine();
+        int id = 0;
+
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(String.format("SELECT USERNAME FROM USER WHER USERNAME = %d", inputUsername));
+        String value ="";
+        if (resultSet.next()) {
+            value = resultSet.getString("USERNAME");
+        }
+        if (inputUsername.equals(value)) {
+            String message = "GEILER SCHEISS!";
+            int i = 0;
+        }
+
+//        statement.executeUpdate(String.format("SELECT USERNAME AND PASSWORD FROM USER WHERE id = %d", id));
 
     }
 
